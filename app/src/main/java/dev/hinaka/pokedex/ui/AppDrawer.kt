b6b22b81c3f4ff04103a33ecda9c48ad8c989a0f
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-06-01 16:12:00 +0700

    add icons for button in drawer

--- app/src/main/java/dev/hinaka/pokedex/ui/AppDrawer.kt
+++ app/src/main/java/dev/hinaka/pokedex/ui/AppDrawer.kt
@@ -1,16 +1,26 @@
 package dev.hinaka.pokedex.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import dev.hinaka.pokedex.R
@@ -32,7 +42,9 @@ fun AppDrawer(
   modifier: Modifier = Modifier,
 ) {
   Column(modifier = modifier.fillMaxSize()) {
+
     DrawerButton(
+      icon = painterResource(id = R.drawable.home_pokedex),
       label = stringResource(id = R.string.home_pokedex),
       isSelected = currentRoute == POKEDEX_ROUTE,
       action = {
@@ -42,6 +54,7 @@ fun AppDrawer(
     )
 
     DrawerButton(
+      icon = painterResource(id = R.drawable.home_move),
       label = stringResource(id = R.string.home_move_dex),
       isSelected = currentRoute == MOVE_ROUTE,
       action = {
@@ -51,6 +64,7 @@ fun AppDrawer(
     )
 
     DrawerButton(
+      icon = painterResource(id = R.drawable.home_ability),
       label = stringResource(id = R.string.home_ability_dex),
       isSelected = currentRoute == ABILITY_ROUTE,
       action = {
@@ -60,6 +74,7 @@ fun AppDrawer(
     )
 
     DrawerButton(
+      icon = painterResource(id = R.drawable.home_item),
       label = stringResource(id = R.string.home_item_dex),
       isSelected = currentRoute == ITEM_ROUTE,
       action = {
@@ -69,6 +84,7 @@ fun AppDrawer(
     )
 
     DrawerButton(
+      icon = painterResource(id = R.drawable.home_nature),
       label = stringResource(id = R.string.home_nature_dex),
       isSelected = currentRoute == NATURE_ROUTE,
       action = {
@@ -81,6 +97,7 @@ fun AppDrawer(
 
 @Composable
 private fun DrawerButton(
+  icon: Painter,
   label: String,
   isSelected: Boolean,
   action: () -> Unit,
@@ -88,7 +105,7 @@ private fun DrawerButton(
 ) {
   val colors = MaterialTheme.colors
 
-  val textColor = if (isSelected) {
+  val iconTextColor = if (isSelected) {
     colors.primary
   } else {
     colors.onSurface.copy(alpha = 0.6f)
@@ -113,11 +130,54 @@ private fun DrawerButton(
       onClick = action,
       modifier = Modifier.fillMaxWidth()
     ) {
-      Text(
-        text = label,
-        style = MaterialTheme.typography.body2,
-        color = textColor,
-      )
+      Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+      ) {
+        DrawerIcon(
+          icon = icon,
+          isSelected = isSelected,
+          contentDescription = label,
+          tintColor = iconTextColor,
+        )
+        Spacer(Modifier.width(16.dp))
+        Text(
+          text = label,
+          style = MaterialTheme.typography.body2,
+          color = iconTextColor,
+        )
+      }
     }
   }
+}
+
+@Composable
+fun DrawerIcon(
+  icon: Painter,
+  isSelected: Boolean,
+  modifier: Modifier = Modifier,
+  contentDescription: String? = null,
+  tintColor: Color? = null,
+) {
+  val imageAlpha = if (isSelected) {
+    1f
+  } else {
+    0.6f
+  }
+
+  val iconTintColor = tintColor ?: if (isSelected) {
+    MaterialTheme.colors.primary
+  } else {
+    MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+  }
+
+  Image(
+    painter = icon,
+    contentDescription = contentDescription,
+    modifier = modifier,
+    contentScale = ContentScale.Inside,
+    colorFilter = ColorFilter.tint(iconTintColor),
+    alpha = imageAlpha
+  )
 }
