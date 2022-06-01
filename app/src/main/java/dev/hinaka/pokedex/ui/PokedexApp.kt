3ecbb6d4b45ae8ae4e099e6d2bbcbe29274b13ac
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-06-01 16:47:25 +0700

    update app color theme

--- app/src/main/java/dev/hinaka/pokedex/ui/PokedexApp.kt
+++ app/src/main/java/dev/hinaka/pokedex/ui/PokedexApp.kt
@@ -1,13 +1,16 @@
 package dev.hinaka.pokedex.ui
 
 import androidx.compose.material.DrawerValue
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalDrawer
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.hinaka.pokedex.ui.PokedexDestinations.ABILITY_ROUTE
 import dev.hinaka.pokedex.ui.PokedexDestinations.ITEM_ROUTE
 import dev.hinaka.pokedex.ui.PokedexDestinations.MOVE_ROUTE
@@ -19,6 +22,12 @@ import kotlinx.coroutines.launch
 @Composable
 fun PokedexApp() {
   PokedexTheme {
+    val systemUiController = rememberSystemUiController()
+    val statusBarColor = MaterialTheme.colors.primaryVariant
+    SideEffect {
+      systemUiController.setStatusBarColor(statusBarColor)
+    }
+
     val coroutineScope = rememberCoroutineScope()
 
     val navController = rememberNavController()
