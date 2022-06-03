6a1b7cb1ebaccd63a13fc58930ba2563bbc0f822
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-06-03 16:48:02 +0700

    apply LazyColumn for PokemonListScreen

--- app/src/main/java/dev/hinaka/pokedex/features/pokemon/ui/list/PokemonListScreen.kt
+++ app/src/main/java/dev/hinaka/pokedex/features/pokemon/ui/list/PokemonListScreen.kt
@@ -1,20 +1,37 @@
 package dev.hinaka.pokedex.ui.pokedex
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.hinaka.pokedex.domain.models.pokemon.Pokemon
 
 @Composable
 fun PokemonListScreen(
   viewModel: PokemonListViewModel,
 ) {
   val pokemons by viewModel.pokemons.collectAsState()
-
-  Column {
-    pokemons.forEach {
-      Text(text = it.name)
-    }
+  LazyColumn(
+    modifier = Modifier
+      .padding(horizontal = 16.dp)
+      .fillMaxSize(),
+  ) {
+    items(
+      items = pokemons,
+      itemContent = {
+        PokemonItem(pokemon = it)
+      }
+    )
   }
+}
+
+@Composable
+fun PokemonItem(pokemon: Pokemon) {
+  Text(text = pokemon.name)
 }
