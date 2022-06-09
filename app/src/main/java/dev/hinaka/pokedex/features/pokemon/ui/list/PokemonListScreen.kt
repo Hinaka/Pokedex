1372f792776c5cdb8e8058eef0fc9839d1581945
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-06-09 16:46:59 +0700

    improve UI for pokemon list screen

--- app/src/main/java/dev/hinaka/pokedex/features/pokemon/ui/list/PokemonListScreen.kt
+++ app/src/main/java/dev/hinaka/pokedex/features/pokemon/ui/list/PokemonListScreen.kt
@@ -1,13 +1,18 @@
 package dev.hinaka.pokedex.features.pokemon.ui.list
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,9 +21,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import dev.hinaka.pokedex.domain.models.pokemon.Pokemon
+import dev.hinaka.pokedex.domain.models.type.Type
 import dev.hinaka.pokedex.ui.pokedex.PokemonListViewModel
 
 @Composable
@@ -39,7 +47,7 @@ fun PokemonListScreen(
 }
 
 @Composable
-fun PokemonItem(pokemon: Pokemon) {
+private fun PokemonItem(pokemon: Pokemon) {
   val surfaceModifier = Modifier
     .padding(start = 8.dp, top = 8.dp, end = 8.dp)
     .fillMaxWidth()
@@ -53,13 +61,68 @@ fun PokemonItem(pokemon: Pokemon) {
       verticalAlignment = Alignment.CenterVertically,
       modifier = Modifier.fillMaxWidth()
     ) {
+      PokemonInfo(pokemon = pokemon, Modifier.weight(1f))
+      Spacer(modifier = Modifier.width(8.dp))
+      PokemonImage(pokemon = pokemon, Modifier.size(80.dp))
+    }
+  }
+}
+
+@Composable
+private fun PokemonInfo(pokemon: Pokemon, modifier: Modifier = Modifier) {
+  Column(modifier = modifier) {
+    Row(modifier = Modifier.fillMaxWidth()) {
       Text(text = "#${pokemon.id}")
-      Text(text = pokemon.name.replaceFirstChar { it.uppercase() })
-      AsyncImage(
-        model = pokemon.sprites.default,
-        contentDescription = pokemon.name,
-        modifier = Modifier.size(64.dp)
+      Spacer(Modifier.width(16.dp))
+      Text(
+        text = pokemon.name.replaceFirstChar { it.uppercase() },
+        modifier = Modifier.weight(1f)
       )
     }
+    Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+      pokemon.types.map {
+        PokemonType(it, Modifier.weight(1f))
+      }
+    }
+  }
+}
+
+@Composable
+private fun PokemonType(type: Type, modifier: Modifier = Modifier) {
+  Surface(
+    modifier = modifier,
+    shape = RoundedCornerShape(8.dp),
+    border = BorderStroke(1.dp, Color.Black),
+  ) {
+    Text(
+      text = type.name.uppercase(),
+      textAlign = TextAlign.Center,
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 4.dp)
+    )
+  }
+}
+
+@Composable
+private fun PokemonImage(pokemon: Pokemon, modifier: Modifier = Modifier) {
+  Surface(
+    shape = RoundedCornerShape(
+      topStartPercent = 50,
+      bottomStartPercent = 50,
+    ),
+    modifier = modifier,
+    color = MaterialTheme.colors.primaryVariant,
+  ) {
+    AsyncImage(
+      model = pokemon.sprites.default,
+      contentDescription = null,
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+    )
   }
 }
