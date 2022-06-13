3468994ffb81735fc1ea37d091503fe157a5f7cf
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-06-13 11:29:00 +0700

    change color of pokemon card based on its type

--- app/src/main/java/dev/hinaka/pokedex/features/pokemon/ui/list/PokemonListScreen.kt
+++ app/src/main/java/dev/hinaka/pokedex/features/pokemon/ui/list/PokemonListScreen.kt
@@ -1,7 +1,6 @@
 package dev.hinaka.pokedex.features.pokemon.ui.list
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -20,28 +19,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.drawable.toBitmap
-import androidx.palette.graphics.Palette
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import coil.size.Size
+import coil.compose.AsyncImage
 import dev.hinaka.pokedex.domain.models.pokemon.Pokemon
 import dev.hinaka.pokedex.domain.models.type.Type
 import dev.hinaka.pokedex.ui.pokedex.PokemonListViewModel
-import kotlinx.coroutines.launch
+import dev.hinaka.pokedex.ui.theme.ofType
 
 @Composable
 fun PokemonListScreen(
@@ -62,29 +52,6 @@ fun PokemonListScreen(
 
 @Composable
 private fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier) {
-  var palette by remember { mutableStateOf<Palette?>(null) }
-
-  val request = ImageRequest.Builder(LocalContext.current)
-    .data(pokemon.sprites.default)
-    .allowHardware(false)
-    .size(Size.ORIGINAL)
-    .build()
-
-  val painter = rememberAsyncImagePainter(model = request)
-  val state = painter.state
-  if (state is AsyncImagePainter.State.Success) {
-    val bitmap = state.result.drawable.toBitmap()
-    LaunchedEffect(key1 = bitmap) {
-      launch {
-        Palette.Builder(bitmap).generate { palette = it }
-      }
-    }
-  }
-
-  val dominantSwatch = palette?.dominantSwatch
-  val dominantColor = dominantSwatch?.rgb?.let { Color(it) } ?: Color.White
-  val textColor = dominantSwatch?.bodyTextColor?.let { Color(it) } ?: Color.White
-
   val surfaceModifier = Modifier
     .padding(start = 8.dp, top = 8.dp, end = 8.dp)
     .fillMaxWidth()
@@ -92,7 +59,7 @@ private fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier) {
   Surface(
     modifier = surfaceModifier,
     shape = RoundedCornerShape(8.dp),
-    color = dominantColor,
+    color = MaterialTheme.colors.ofType(pokemon.types.first()).copy(alpha = 0.7f),
   ) {
     Row(
       horizontalArrangement = Arrangement.Start,
@@ -105,9 +72,9 @@ private fun PokemonItem(pokemon: Pokemon, modifier: Modifier = Modifier) {
       PokemonInfo(pokemon = pokemon,
         Modifier
           .weight(1f)
-          .padding(vertical = 8.dp), textColor)
+          .padding(vertical = 8.dp), Color.White)
       Spacer(modifier = Modifier.width(8.dp))
-      PokemonImage(painter,
+      PokemonImage(pokemon,
         Modifier
           .width(80.dp)
           .fillMaxHeight())
@@ -149,8 +116,7 @@ private fun PokemonType(type: Type, modifier: Modifier = Modifier, textColor: Co
   Surface(
     modifier = modifier,
     shape = RoundedCornerShape(16.dp),
-    border = BorderStroke(1.dp, textColor),
-    color = Color.Transparent,
+    color = MaterialTheme.colors.ofType(type),
   ) {
     Text(
       text = type.name.uppercase(),
@@ -165,7 +131,7 @@ private fun PokemonType(type: Type, modifier: Modifier = Modifier, textColor: Co
 }
 
 @Composable
-private fun PokemonImage(painter: AsyncImagePainter, modifier: Modifier = Modifier) {
+private fun PokemonImage(pokemon: Pokemon, modifier: Modifier = Modifier) {
   Surface(
     shape = RoundedCornerShape(
       topStartPercent = 50,
@@ -174,6 +140,8 @@ private fun PokemonImage(painter: AsyncImagePainter, modifier: Modifier = Modifi
     color = Color.White.copy(0.4f),
     modifier = modifier
   ) {
-    Image(painter = painter, contentDescription = null, modifier = Modifier.padding(4.dp))
+    AsyncImage(model = pokemon.sprites.default,
+      contentDescription = null,
+      modifier = Modifier.padding(4.dp))
   }
 }
--- app/src/main/java/dev/hinaka/pokedex/ui/theme/Theme.kt
+++ app/src/main/java/dev/hinaka/pokedex/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package dev.hinaka.pokedex.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import dev.hinaka.pokedex.domain.models.type.Type
+import dev.hinaka.pokedex.domain.models.type.Type.*
 
 private val DarkColorPalette = darkColors(
   primary = Red200,
@@ -39,4 +43,26 @@ fun PokedexTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
     shapes = Shapes,
     content = content
   )
+}
+
+fun Colors.ofType(type: Type) = when (type) {
+  NORMAL -> Color(0xFFA0A29F)
+  FIGHTING -> Color(0xFFD3425F)
+  FLYING -> Color(0xFFA1BBEC)
+  POISON -> Color(0xFFB763CF)
+  GROUND -> Color(0xFFDA7C4D)
+  ROCK -> Color(0xFFC9BB8A)
+  BUG -> Color(0xFF92BC2C)
+  GHOST -> Color(0xFF5F6DBC)
+  STEEL -> Color(0xFF5695A3)
+  FIRE -> Color(0xFFFBA54C)
+  WATER -> Color(0xFF539DDF)
+  GRASS -> Color(0xFF5FBD58)
+  ELECTRIC -> Color(0xFFF2D94E)
+  PSYCHIC -> Color(0xFFFA8581)
+  ICE -> Color(0xFF75D0C1)
+  DRAGON -> Color(0xFF0C69C8)
+  DARK -> Color(0xFF595761)
+  FAIRY -> Color(0xFFEE90E6)
+  UNKNOWN -> Color(0xFFCBD4DD)
 }
